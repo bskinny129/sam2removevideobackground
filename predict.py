a739652d9542949837b084ccc4d502fd48b08764
bskinny129 <brian.skinner9@gmail.com>
2025-06-10 17:06:01 -0700

    revert

--- predict.py
+++ predict.py
@@ -34,14 +34,7 @@ class Predictor(BasePredictor):
         self.checkpoint = "sam2_hiera_base_plus.pt"
         self.model_cfg = "sam2.1_hiera_b+.yaml"
 
-        if self.device == "cpu":
-            self.predictor = build_sam2_video_predictor(
-                self.model_cfg,
-                self.checkpoint,
-                overrides=["device=cpu"]
-            )
-        else:
-            self.predictor = build_sam2_video_predictor(self.model_cfg, self.checkpoint)
+        self.predictor = build_sam2_video_predictor(self.model_cfg, self.checkpoint)
         logging.info("SAM2 predictor built successfully")
 
         # pre‑trained Faster R‑CNN for body detection
